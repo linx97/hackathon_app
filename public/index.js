378c35006ff9bfdeb085a149a6e8c23e32dbd3ab
Linx Latham <linxlatham@gmail.com>
2016-10-20 12:23:02 -0600

    ✨word cloud

---
+++ public/index.js
@@ -0,0 +1,16 @@
+var wordList = [
+       {text: "Lorem", weight: 10},
+       {text: "Ipsum", weight: 9},
+       {text: "Dolor", weight: 10},
+       {text: "Sit", weight: 9},
+       {text: "Amet", weight: 10},
+       {text: "Lorem", weight: 10},
+       {text: "Ipsum", weight: 9},
+       {text: "Dolor", weight: 10},
+       {text: "Sit", weight: 9},
+       {text: "Amet", weight: 10}
+    ];
+
+$(document).ready(function() {
+    $('.frame').jQCloud(wordList);
+});
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(function(req, res, next) {
 });
 
 app.get("/", function(req, res) {
-	res.sendFile(__dirname + "/index.html");
+	res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/api", function(req, res) {
