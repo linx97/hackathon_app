39b29505d0735e0e2c791bb517dbd46c37e1d249
Linx Latham <linxlatham@gmail.com>
2016-10-20 15:30:45 -0600

    ✨added drag and drop

--- public/index.js
+++ public/index.js
@@ -1,16 +1,47 @@
-var wordList = [
-       {text: "Lorem", weight: 10},
-       {text: "Ipsum", weight: 9},
-       {text: "Dolor", weight: 10},
-       {text: "Sit", weight: 9},
-       {text: "Amet", weight: 10},
-       {text: "Lorem", weight: 10},
-       {text: "Ipsum", weight: 9},
-       {text: "Dolor", weight: 10},
-       {text: "Sit", weight: 9},
-       {text: "Amet", weight: 10}
+/* jshint esversion:6 */
+$(document).ready(function() {
+  var wordList = [
+       {text: "thing", weight: 10, html: {class: "draggable"}},
+       {text: "Ipsum", weight: 9, html: {class: "draggable"}},
+       {text: "Dolor", weight: 10, html: {class: "draggable"}},
+       {text: "Sit", weight: 9, html: {class: "draggable"}},
+       {text: "Amet", weight: 10, html: {class: "draggable"}},
+       {text: "Lorem", weight: 10, html: {class: "draggable"}},
+       {text: "Ipsum", weight: 9, html: {class: "draggable"}},
+       {text: "Dolor", weight: 10, html: {class: "draggable"}},
+       {text: "Sit", weight: 9, html: {class: "draggable"}},
+       {text: "Amet", weight: 10, html: {class: "draggable"}}
     ];
 
-$(document).ready(function() {
-    $('.frame').jQCloud(wordList);
+    $('.frame').jQCloud(wordList, {
+      autoResize: true,
+      afterCloudRender: function() {
+        $('.draggable').draggable();
+      }
+    });
+
+    $( function() {
+      
+      $( ".droppable" ).droppable({
+        drop: function( event, ui ) {
+          $( this )
+            .addClass( "ui-state-highlight" )
+            .find( "p" )
+              .html( "Dropped!" );
+        }
+      });
+      $( ".droppable2" ).droppable({
+        drop: function( event, ui ) {
+          $( this )
+            .addClass( "ui-state-highlight" )
+            .find( "p" )
+              .html( "Dropped!" );
+        }
+    });
+  });
+
+
+
+
 });
+
