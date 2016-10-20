03b7dfb6b58411eb9b728737f97bcc417406d64d
mikeryanolson <mikeryanolson@gmail.com>
2016-10-20 15:53:40 -0600

    molson-test

--- public/index.js
+++ public/index.js
@@ -1,47 +1,44 @@
-/* jshint esversion:6 */
+ /*jshint-esversion6*/
+
 $(document).ready(function() {
-  var wordList = [
-       {text: "thing", weight: 10, html: {class: "draggable"}},
-       {text: "Ipsum", weight: 9, html: {class: "draggable"}},
-       {text: "Dolor", weight: 10, html: {class: "draggable"}},
-       {text: "Sit", weight: 9, html: {class: "draggable"}},
-       {text: "Amet", weight: 10, html: {class: "draggable"}},
-       {text: "Lorem", weight: 10, html: {class: "draggable"}},
-       {text: "Ipsum", weight: 9, html: {class: "draggable"}},
-       {text: "Dolor", weight: 10, html: {class: "draggable"}},
-       {text: "Sit", weight: 9, html: {class: "draggable"}},
-       {text: "Amet", weight: 10, html: {class: "draggable"}}
-    ];
+ var wordList = [
+      {text: "thing", weight: 20, html: {class: "draggable"}},
+      {text: "Ipsum", weight: 20, html: {class: "draggable"}},
+      {text: "Dolor", weight: 20, html: {class: "draggable"}},
+      {text: "Sit", weight: 20, html: {class: "draggable"}},
+      {text: "Amet", weight: 20, html: {class: "draggable"}},
+      {text: "Lorem", weight: 20, html: {class: "draggable"}},
+      {text: "Dolor", weight: 20, html: {class: "draggable"}},
+      {text: "Sit", weight: 20, html: {class: "draggable"}},
+      {text: "Amet", weight: 20, html: {class: "draggable"}}
+   ];
 
-    $('.frame').jQCloud(wordList, {
-      autoResize: true,
-      afterCloudRender: function() {
-        $('.draggable').draggable();
-      }
-    });
+   $('.frame').jQCloud(wordList, {
+     shape: 'rectangular',
+     autoResize: true,
+     afterCloudRender: function() {
+       $('.draggable').draggable();
+     }
+   });
 
-    $( function() {
-      
-      $( ".droppable" ).droppable({
-        drop: function( event, ui ) {
-          $( this )
-            .addClass( "ui-state-highlight" )
-            .find( "p" )
-              .html( "Dropped!" );
-        }
-      });
-      $( ".droppable2" ).droppable({
-        drop: function( event, ui ) {
-          $( this )
-            .addClass( "ui-state-highlight" )
-            .find( "p" )
-              .html( "Dropped!" );
-        }
-    });
-  });
-
-
-
+   $( function() {
+     
+     $( ".droppable" ).droppable({
+       drop: function( event, ui ) {
+         $( this )
+           .addClass( "ui-state-highlight" )
+           .find( "p" )
+             .html( "Dropped!" );
+       }
+     });
+     $( ".droppable2" ).droppable({
+       drop: function( event, ui ) {
+         $( this )
+           .addClass( "ui-state-highlight" )
+           .find( "p" )
+             .html( "Dropped!" );
+       }
+   });
+ });
 
 });
-
