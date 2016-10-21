eb92d1502b4a68d7e0bf508129d4c95fcbf9a123
Linx Latham <linxlatham@gmail.com>
2016-10-21 09:27:01 -0600

    ✨Added buckets

--- public/index.js
+++ public/index.js
@@ -1,22 +1,12 @@
 /* jshint esversion:6 */
 $(document).ready(function() {
-  // var wordList = [
-  //      {text: "thing", weight: 20, html: {class: "draggable"}},
-  //      {text: "Ipsum", weight: 20, html: {class: "draggable"}},
-  //      {text: "Dolor", weight: 20, html: {class: "draggable"}},
-  //      {text: "Sit", weight: 20, html: {class: "draggable"}},
-  //      {text: "Amet", weight: 20, html: {class: "draggable"}},
-  //      {text: "Lorem", weight: 20, html: {class: "draggable"}},
-  //      {text: "Dolor", weight: 20, html: {class: "draggable"}},
-  //      {text: "Sit", weight: 20, html: {class: "draggable"}},
-  //      {text: "Amet", weight: 20, html: {class: "draggable"}}
-  //   ];
 
   $.get('/api/wordlist', function(response) {
     var wordList = response;
     console.log(wordList);
 
-    $('.frame').jQCloud(wordList, {
+    $('.word-div').jQCloud(wordList, {
+
       autoResize: true,
       colors: ["#9932cc", "#9932cc", "#A33FD0", "#B148D6", "#BD55E1", "#C85EE7"],
       afterCloudRender: function() {
