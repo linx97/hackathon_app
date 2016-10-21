5e84daf9193bea98d4d7afe64831b6141984c8cb
Linx Latham <linxlatham@gmail.com>
2016-10-21 15:37:00 -0600

    💅bin logos

--- public/index.js
+++ public/index.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
     var wordList = response;
    var score = 0;
    $('.word-div').jQCloud(wordList, {
-
       autoResize: true,
       colors: ["#9932cc", "#9932cc", "#A33FD0", "#B148D6", "#BD55E1", "#C85EE7"],
       afterCloudRender: function() {
