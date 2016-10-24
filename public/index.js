3b22d0b7930000a061ba95f2500eb740242c2169
britdavis <brit@britdavis.com>
2016-10-24 09:51:59 -0600

    cleaned up code

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 
   $.get('/api/wordlist', function(response) {
     var wordList = response;
-   var score = 0;
+    var score = 0;
    $('.word-div').jQCloud(wordList, {
       autoResize: true,
       colors: ["#A554C0"],
@@ -15,19 +15,15 @@ $(document).ready(function() {
         $( function() {
           $( ".droppable" ).droppable({
             drop: function( event, ui ) {
-              $( this )
-
-              .find( "p" );
+              $( this ).find( "p" );
               var bin = event.target;
               var word = ui.draggable;
-          
               var wordId = word[0].classList[1];
               if (wordId === bin.id) {
                 score = score +100;
                 $('#score').text(score);
                 $('#graphic').text("👍").fadeTo(500, 1).fadeTo(1000, 0);
                 
-
               } else {
                 score = score -50;
                 $('#score').text(score);
