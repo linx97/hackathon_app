e1c162f93559de1e0c56ace543d6aa5da50dd84a
Linx Latham <linxlatham@gmail.com>
2016-10-24 14:54:14 -0600

    idk

--- Words.js
+++ Words.js
@@ -8,17 +8,16 @@ var getRandom = function(arr) {
 var Words = function(data) {
 	var output = [];
 	for (var a in data) {
+		var check = [];
 		for (var i = 0; i < 6; i++) {
 			var index = getRandom(data[a]);
-			console.log(data[a][index]);
-			if (!output.includes(data[a][index])) {
-
+			if (!check.includes(data[a][index])) {
 				var j = new Word(data[a][index], a);
 				output.push(j);
+				check.push(data[a][index]);
 			}
 		}
 	}
-
 	// console.log(output);
 	return output;
 };
